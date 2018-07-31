9bf2ab9dc7cc62326cf085724ae9ef4d42da96d8
Jaron Rosenau <jaron.rosenau@gmail.com>
2018-07-30 16:00:27 -0800

    Update HtmlcoinTestNetParams.java

--- core/src/main/java/org/bitcoinj/params/HtmlcoinTestNetParams.java
+++ core/src/main/java/org/bitcoinj/params/HtmlcoinTestNetParams.java
@@ -1,26 +1,27 @@
 package org.bitcoinj.params;
 
-import org.bitcoinj.core.*;
-import org.bitcoinj.net.discovery.*;
-import org.bitcoinj.params.AbstractBitcoinNetParams;
+import org.bitcoinj.core.ECKey;
+import org.bitcoinj.core.Sha256Hash;
+import org.bitcoinj.core.Utils;
+import org.bitcoinj.net.discovery.HttpDiscovery;
 
-import java.net.*;
+import java.net.URI;
 
-import static com.google.common.base.Preconditions.*;
+import static com.google.common.base.Preconditions.checkState;
 
 
-public class HtmlcoinTestNetParams extends AbstractBitcoinNetParams {
+public class HtmlCoinTestNetParams extends AbstractBitcoinNetParams {
     public static final int MAINNET_MAJORITY_WINDOW = 1000;
     public static final int MAINNET_MAJORITY_REJECT_BLOCK_OUTDATED = 950;
     public static final int MAINNET_MAJORITY_ENFORCE_BLOCK_UPGRADE = 750;
 
-    public HtmlcoinTestNetParams() {
+    public HtmlCoinTestNetParams() {
         super();
         interval = INTERVAL;
         targetTimespan = TARGET_TIMESPAN;
         maxTarget = Utils.decodeCompactBits(0x1d00ffffL);
-        dumpedPrivateKeyHeader = 238;
-        addressHeader = 100;
+        dumpedPrivateKeyHeader = 239;
+        addressHeader = 120;
         p2shHeader = 110;
         acceptableAddressCodes = new int[] { addressHeader, p2shHeader };
         port = 14888;
@@ -32,29 +33,24 @@ public class HtmlcoinTestNetParams extends AbstractBitcoinNetParams {
         majorityRejectBlockOutdated = MAINNET_MAJORITY_REJECT_BLOCK_OUTDATED;
         majorityWindow = MAINNET_MAJORITY_WINDOW;
 
-        genesisBlock.setDifficultyTarget(0x1d00ffffL);
+        genesisBlock.setDifficultyTarget(0x1f00ffffL);
         genesisBlock.setTime(1506212200L);
         genesisBlock.setNonce(102232);
         id = ID_HTML_TESTNET;
         subsidyDecreaseBlockCount = 210000;
         spendableCoinbaseDepth = 100;
         String genesisHash = genesisBlock.getHashAsString();
-        //checkState(genesisHash.equals("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"),
-        //        genesisHash);
+//        checkState(genesisHash.equals("000013694772f8aeb88efeb2829fe5d71fbca3e23d5043baa770726f204f528c"), genesisHash);
 
         // This contains (at a minimum) the blocks which are not BIP30 compliant. BIP30 changed how duplicate
         // transactions are handled. Duplicated transactions could occur in the case where a coinbase had the same
         // extraNonce and the same outputs but appeared at different heights, and greatly complicated re-org handling.
         // Having these here simplifies block connection logic considerably.
-        //checkpoints.put(91722, Sha256Hash.wrap("00000000000271a2dc26e7667f8419f2e15416dc6955e5a6c6cdf3f2574dd08e"));
-        //checkpoints.put(91812, Sha256Hash.wrap("00000000000af0aed4792b1acee3d966af36cf5def14935db8de83d6f9306f2f"));
-        //checkpoints.put(91842, Sha256Hash.wrap("00000000000a4d0a398161ffc163c503763b1f4360639393e0e4c8e300e0caec"));
-        //checkpoints.put(91880, Sha256Hash.wrap("00000000000743f190a18c5577a3c2d2a1f610ae9601ac046a38084ccb7cd721"));
         checkpoints.put(0, Sha256Hash.wrap("000013694772f8aeb88efeb2829fe5d71fbca3e23d5043baa770726f204f528c"));
 
         dnsSeeds = new String[] {
                 "testnet-seed1.htmlcoin.com",
-                "testnet-seed2.htmlcoin.com",
+                "testnet-seed2.htmlcoin.com"
         };
         httpSeeds = new HttpDiscovery.Details[] {
                 // Andreas Schildbach
@@ -108,10 +104,10 @@ public class HtmlcoinTestNetParams extends AbstractBitcoinNetParams {
         };
     }
 
-    private static HtmlcoinTestNetParams instance;
-    public static synchronized HtmlcoinTestNetParams get() {
+    private static HtmlCoinTestNetParams instance;
+    public static synchronized HtmlCoinTestNetParams get() {
         if (instance == null) {
-            instance = new HtmlcoinTestNetParams();
+            instance = new HtmlCoinTestNetParams();
         }
         return instance;
     }
